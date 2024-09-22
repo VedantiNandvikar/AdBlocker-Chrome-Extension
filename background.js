755213a4e9d070126792f875785b279750eb8b56
VedantiNandvikar <131340275+VedantiNandvikar@users.noreply.github.com>
2024-09-22 19:16:44 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,20 @@
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+    "*://*.ads.google.com/*",
+    "*://*.adsense.google.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.adpushup.com/*",
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
